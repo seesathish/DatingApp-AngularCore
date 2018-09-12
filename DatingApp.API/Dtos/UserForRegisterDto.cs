5eddae216063e2b3548362a8b2ef8f91a21e2341
Sathish <seesathish@gmail.com>
2018-09-12 12:49:56 +0530

    Authentication with JWT Token

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must give minimum 4 letters")]
+        public string Password { get; set; }
+    }
+}
